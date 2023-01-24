eeaaccc2f64dcc1ae1b986fb9db32ed0f3c697d7
Shelwin <shelwinsunga@gmail.com>
2023-01-24 13:59:19 -0600

    cleaned swr

--- theme.config.js
+++ theme.config.js
@@ -1,13 +1,6 @@
 import { useRouter } from "next/router";
 
-const Logo = ({ height }) => (
-  <svg height={height} viewBox="0 0 291 69" fill="none">
-    <path
-      d="M0 36.53c.07 17.6 14.4 32.01 32.01 32.01a32.05 32.05 0 0032.01-32V32a13.2 13.2 0 0123.4-8.31h20.7A32.07 32.07 0 0077.2 0a32.05 32.05 0 00-32 32.01v4.52A13.2 13.2 0 0132 49.71a13.2 13.2 0 01-13.18-13.18 3.77 3.77 0 00-3.77-3.77H3.76A3.77 3.77 0 000 36.53zM122.49 68.54a32.14 32.14 0 01-30.89-23.7h20.67a13.16 13.16 0 0023.4-8.3V32A32.05 32.05 0 01167.68 0c17.43 0 31.64 14 32 31.33l.1 5.2a13.2 13.2 0 0023.4 8.31h20.7a32.07 32.07 0 01-30.91 23.7c-17.61 0-31.94-14.42-32.01-32l-.1-4.7v-.2a13.2 13.2 0 00-13.18-12.81 13.2 13.2 0 00-13.18 13.18v4.52a32.05 32.05 0 01-32.01 32.01zM247.94 23.7a13.16 13.16 0 0123.4 8.31 3.77 3.77 0 003.77 3.77h11.3a3.77 3.77 0 003.76-3.77A32.05 32.05 0 00258.16 0a32.07 32.07 0 00-30.92 23.7h20.7z"
-      fill="currentColor"
-    />
-  </svg>
-);
+
 
 const Vercel = ({ height = 20 }) => (
   <svg height={height} viewBox="0 0 283 64" fill="none">
@@ -35,9 +28,9 @@ const FEEDBACK_LINK_WITH_TRANSLATIONS = {
 };
 
 export default {
-  projectLink: "https://github.com/vercel/swr",
+  projectLink: "https://github.com/shelwinsunga/nagios-products-docs",
   docsRepositoryBase: "https://github.com/vercel/swr-site/blob/master/pages",
-  titleSuffix: " – SWR",
+  titleSuffix: " – Nagios",
   search: true,
   unstable_flexsearch: true,
   floatTOC: true,
@@ -53,12 +46,9 @@ export default {
     const { locale } = useRouter();
     return (
       <>
-        <Logo height={12} />
         <span
-          className="mx-2 font-extrabold hidden md:inline select-none"
-          title={"SWR: " + (TITLE_WITH_TRANSLATIONS[locale] || "")}
-        >
-          SWR
+          className="mx-2 font-extrabold hidden md:inline select-none">
+          Nagios
         </span>
       </>
     );
@@ -66,66 +56,9 @@ export default {
   head: ({ title, meta }) => {
     const { route } = useRouter();
 
-    const ogImage =
-      meta.image ||
-      `https://swr-card.vercel.app${
-        /\/index\.+/.test(route) ? "" : "?title=" + encodeURIComponent(title)
-      }`;
-
     return (
       <>
-        {/* Favicons, meta */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <meta
-          name="description"
-          content={
-            meta.description ||
-            "SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again."
-          }
-        />
-        <meta
-          name="og:description"
-          content={
-            meta.description ||
-            "SWR is a React Hooks library for data fetching. SWR first returns the data from cache (stale), then sends the fetch request (revalidate), and finally comes with the up-to-date data again."
-          }
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@vercel" />
-        <meta name="twitter:image" content={ogImage} />
-        <meta
-          name="og:title"
-          content={
-            title ? title + " – SWR" : "SWR: React Hooks for Data Fetching"
-          }
-        />
-        <meta name="og:image" content={ogImage} />
-        <meta name="apple-mobile-web-app-title" content="SWR" />
+
       </>
     );
   },
@@ -236,17 +169,7 @@ export default {
         );
       default:
         return (
-          <a
-            href="https://vercel.com/?utm_source=swr"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-1">Powered by</span>
-            <span>
-              <Vercel />
-            </span>
-          </a>
+          <span className="mr-1">Initiated by Shelwin</span>
         );
     }
   },
